6ec4829af66c9d7973997b9d5a642c67a2565fbc
Skylar Yi Li <yil067@ucsd.edu>
2021-02-26 14:01:29 +0800

    done

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key ='jtPlDBK3qHe59Tuwvo8NrEHLnbCGpZXM';
+
+	var map = L.mapquest.map('map',{
+		center:[32.87920477741168, -117.23618644754849],
+		layers:L.mapquest.tileLayer('map'),
+		zoom:12,
+		zoomControl:false
+	});
+
+L.marker([32.87920477741168, -117.23618644754849]).addTo(map);
 }
